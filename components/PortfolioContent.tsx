fd37f73f2e6474d85ed727e4bf43adf9d9d160e5
davincodes <aarvinantonio@gmail.com>
2026-02-28 17:39:40 +0800

    contact sections and notifs to back end

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,21 +1,19 @@
 import { HeroSection } from "./sections/HeroSection";
 import { AboutSection } from "./sections/AboutSection";
+import { ExperienceSection } from "./sections/ExperienceSection";
+import { EducationSection } from "./sections/EducationSection";
+import { ProjectsSection } from "./sections/ProjectsSection";
+import { ContactSection } from "./sections/ContactSection";
 
 async function PortfolioContent() {
   return (
     <>
       <HeroSection />
       <AboutSection />
-      {/* <TestimonialsSection />
-      <SkillsSection />
       <ExperienceSection />
       <EducationSection />
       <ProjectsSection />
-      <CertificationsSection />
-      <AchievementsSection />
-      <ServicesSection />
-      <BlogSection />
-      <ContactSection /> */}
+      <ContactSection />
     </>
   );
 }
