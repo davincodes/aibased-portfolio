71715ea2fffee61a9d53e7054a6b93fd40f1a68b
davincodes <aarvinantonio@gmail.com>
2026-02-28 10:44:20 +0800

    Building the “Chat with my AI Twin” Functionality

---
+++ components/chat/Chat.tsx
@@ -0,0 +1,11 @@
+import type { CHAT_PROFILE_QUERYResult } from "@/sanity.types";
+
+export function Chat({
+  profile,
+}: {
+  profile: CHAT_PROFILE_QUERYResult | null;
+}) {
+  return <div>Chat</div>;
+}
+
+export default Chat;
